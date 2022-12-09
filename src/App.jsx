b2732b772614cb6418cbcfc22238516ea492ce56
romoguill <romoguill@gmail.com>
2022-12-09 10:01:16 -0300

    Finished the routing of the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact"
+import Detail from "./Routes/Detail"
+import Favs from "./Routes/Favs"
+import NoPage from "./Routes/NoPage";
 
 
 function App() {
   return (
       <div className="App">
           <Navbar/>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/contacto" element={<Contact/>} />
+              <Route path="/dentista:id" element={<Detail/>} />
+              <Route path="/favoritos" element={<Favs/>} />
+              <Route path="*" element={<NoPage />} />
+            </Routes>
+          </BrowserRouter>
           <Footer/>
       </div>
   );
---
+++ src/Routes/NoPage.jsx
@@ -0,0 +1,7 @@
+const NoPage = () => {
+  return (
+    <div>404 - Pagina no encontrada</div>
+  )
+}
+
+export default NoPage
