f295c17e8993265a8a48b7684d41aa14914a65ab
romoguill <romoguill@gmail.com>
2022-12-09 20:03:28 -0300

    Created Favs and Home pages

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import Card from "../Components/Card";
+import React from 'react';
+import Card from '../Components/Card';
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
+  const favs = JSON.parse(localStorage.getItem('favs'));
 
   return (
     <>
@@ -11,6 +12,14 @@ const Favs = () => {
       <div className="card-grid">
         {/* este componente debe consumir los destacados del localStorage */}
         {/* Deberan renderizar una Card por cada uno de ellos */}
+        {favs.map((fav) => (
+          <Card
+            key={fav.id}
+            name={fav.name}
+            username={fav.username}
+            id={fav.id}
+          />
+        ))}
       </div>
     </>
   );
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
-import Card from '../Components/Card'
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Card from '../Components/Card';
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
+  const [dentistas, setDentistas] = useState([]);
+
+  const getDentistas = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    setDentistas(data);
+  };
+
+  useEffect(() => {
+    getDentistas();
+  }, []);
+
   return (
-    <main className="" >
+    <main className="">
       <h1>Home</h1>
-      <div className='card-grid'>
+      <div className="card-grid">
         {/* Aqui deberias renderizar las cards */}
+        {dentistas.map((dentista) => (
+          <Card
+            key={dentista.id}
+            name={dentista.name}
+            username={dentista.username}
+            id={dentista.id}
+          />
+        ))}
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
