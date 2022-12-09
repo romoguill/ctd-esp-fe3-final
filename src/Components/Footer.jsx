7f001036ae93385d02a27f409e8ba4111b7847d8
romoguill <romoguill@gmail.com>
2022-12-09 10:14:39 -0300

    Finished footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
+      <div className='brand'>
         <p>Powered by</p>
         <img src="./img/DH.png" alt='DH-logo' />
+      </div>
+      <div className='social'>
+        <img src='images/ico-facebook.png' alt='facebook-logo' />
+        <img src='images/ico-instagram.png' alt='instagram-logo' />
+        <img src='images/ico-whatsapp.png' alt='whatsapp-logo' />
+        <img src='images/ico-tiktok.png' alt='tiktok-logo' />
+      </div>
     </footer>
   )
 }
