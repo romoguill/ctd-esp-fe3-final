e3d07492fd5f16f009fd5315b6513985fb2a935e
romoguill <romoguill@gmail.com>
2022-12-09 20:13:40 -0300

    Finished Detail Page

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,19 +1,54 @@
-import React from 'react'
-
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Detail = () => {
- 
+  const id = useParams();
+
+  const [dentista, setDentista] = useState(undefined);
+
   // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
+  const getDentista = async () => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    const data = await response.json();
+    setDentista(data);
+  };
+
+  useEffect(() => {
+    getDentista();
+  }, []);
 
   return (
     <>
-      <h1>Detail Dentist id </h1>
+      <h1>Detail Dentist {id} </h1>
       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
       {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
-    </>
-  )
-}
+      <table className="tabla-detalle">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Website</th>
+          </tr>
+        </thead>
 
-export default Detail
+        <tbody>
+          <tr>
+            <td>{dentista.name}</td>
+            <td>{dentista.email}</td>
+            <td>{dentista.phone}</td>
+            <td>{dentista.website}</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Detail;
